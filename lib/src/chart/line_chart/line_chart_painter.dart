a620ec0d105d07964689ec2a80357d047ce4b07a
= <jamesnicholls04@gmail.com>
2020-10-07 23:50:28 +0100

    sd

--- lib/src/chart/line_chart/line_chart_painter.dart
+++ lib/src/chart/line_chart/line_chart_painter.dart
@@ -1264,10 +1264,10 @@ class LineChartPainter extends AxisChartPainter<LineChartData>
     //TODO: this is james and again a bit of a guess
     final Path _path = Path();
     final Rect myRect =
-        Rect.fromLTRB(rect.left - 10, rect.top - 10, rect.right - 10, rect.bottom - 10);
+        Rect.fromLTRB(rect.left - 10, rect.top - 10, rect.right + 10, rect.bottom + 10);
     final RRect myRRect = RRect.fromRectAndRadius(myRect, radius);
     _path.addRRect(myRRect);
-    canvas.drawShadow(_path, shadowColor, 0, false);
+    canvas.drawShadow(_path, shadowColor, 1, false);
 
     // /// draw the texts one by one in below of each other
     double topPosSeek = tooltipData.tooltipPadding.top;
